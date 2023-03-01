4f8320706e66ccb2c82815405a6c915953d67f98
ry111888 <renyi2021fall@163.com>
2023-03-01 16:39:40 -0600

    My react todoList app

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,14 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
+import {Provider} from "react-redux";
+import store from './store'
 
 test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  render(
+      <Provider store={store}>
+        <App />
+      </Provider>
+  )
+  const linkElement = screen.getByText(/Log in/i);
   expect(linkElement).toBeInTheDocument();
 });
